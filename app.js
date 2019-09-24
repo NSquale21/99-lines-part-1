b7aea65808541920cd0b7b5396eb2397d3b6f2fd
Nicole Pasquale <NSquale212@gmail.com>
2019-09-24 16:54:14 -0500

    Update console.log message

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ for (let i = 0; i < friends.length; i++) {
 	console.log(`${friends[i].toUpperCase()}:`);
   for (let count = 99; count >= 1; count--) {
 		if (count === 1) {
-			console.log(`${count} line of code in the file, ${count} line of code; ${friends[i]} strikes one out, clears it all out, no lines of code in the file`)
+			console.log(`${count} line of code in the file, ${count} line of code; ${friends[i]} strikes one out, clears it all out, no more lines of code in the file`)
 		} else {
 			console.log(`${count} lines of code in the file, ${count} lines of code; ${friends[i]} strikes one out, clears it all out, ${count - 1} lines of code in the file`);
 		}

8b79a9f62cfaf7207fab768560c866bbfcc81c0a
Nicole Pasquale <NSquale212@gmail.com>
2019-09-24 16:48:14 -0500

    Update app.js & link files

---
+++ app.js
@@ -0,0 +1,12 @@
+let friends = ['Tony', 'Shannon', 'Kim', 'Shelby', 'Lorenz'];
+
+for (let i = 0; i < friends.length; i++) {
+	console.log(`${friends[i].toUpperCase()}:`);
+  for (let count = 99; count >= 1; count--) {
+		if (count === 1) {
+			console.log(`${count} line of code in the file, ${count} line of code; ${friends[i]} strikes one out, clears it all out, no lines of code in the file`)
+		} else {
+			console.log(`${count} lines of code in the file, ${count} lines of code; ${friends[i]} strikes one out, clears it all out, ${count - 1} lines of code in the file`);
+		}
+	}
+}

1bf6cc8fca5b31c354281b43b1c1bc7552e283af
Nicole Pasquale <NSquale212@gmail.com>
2019-09-27 14:54:01 -0500

    Add else if & console.group

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let friends = ['Tony', 'Shannon', 'Kim', 'Shelby', 'Lorenz'];
 
 for (let i = 0; i < friends.length; i++) {
-	console.log(`${friends[i].toUpperCase()}:`);
-  for (let count = 99; count >= 1; count--) {
+	console.group(`${friends[i].toUpperCase()}`);
+  for (let count = 99; count > 0; count--) {
 		if (count === 1) {
 			console.log(`${count} line of code in the file, ${count} line of code; ${friends[i]} strikes one out, clears it all out, no more lines of code in the file`)
+		} else if (count === 2) {
+			console.log(`${count} lines of code in the file, ${count} lines of code; ${friends[i]} strikes one out, clears it all out, ${count - 1} line of code in the file`);
 		} else {
-			console.log(`${count} lines of code in the file, ${count} lines of code; ${friends[i]} strikes one out, clears it all out, ${count - 1} lines of code in the file`);
+			console.log(`${count} lines of code in the file, ${count} lines of code; ${friends[i]} strikes one out, clears it all out, ${count - 1} lines of code in the file`)
 		}
 	}
+	console.groupEnd();
 }
